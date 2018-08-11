f1d31579b3f182463b49820b060641e7e29e3f14
Song Intae <>
2018-08-12 00:47:59 +0900

    initailize

---
+++ src/main/java/TravisCI/TravisCiApplication.java
@@ -0,0 +1,12 @@
+package TravisCI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravisCiApplication.class, args);
+	}
+}
---
+++ src/test/java/TravisCI/TravisCiApplicationTests.java
@@ -0,0 +1,16 @@
+package TravisCI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravisCiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
